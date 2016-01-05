2a38bb49d1805dbf1be4d696232af8c38f320bc8
Jose Garcia-Sacristan <jose.garciascristan@gmail.com>
2016-01-05 18:28:35 +0000

    persist annotations

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -25,8 +25,8 @@ defmodule Rumbl.UserSocket do
   def connect(%{"token" => token}, socket) do
 		case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
       {:ok, user_id} ->
-				# user = Rumbl.Repo.get!(Rumbl.User, user_id)
-				{:ok, assign(socket, :user_id, user_id)}
+				user = Rumbl.Repo.get!(Rumbl.User, user_id)
+				{:ok, assign(socket, :current_user, user)}
 			{:error, _reason} ->
 				:error
 		end
@@ -44,5 +44,5 @@ defmodule Rumbl.UserSocket do
   #     Rumbl.Endpoint.broadcast("users_socket:" <> user.id, "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
+  def id(socket), do: "users_socket:#{socket.assigns.current_user.id}"
 end
