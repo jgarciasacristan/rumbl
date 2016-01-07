8f2b46a152323e2bf85d64bade3789d9b5a982eb
Jose Garcia-Sacristan <jose.garciascristan@gmail.com>
2016-01-07 23:22:19 +0000

    integrate wolfram stuff

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -17,23 +17,44 @@ defmodule Rumbl.VideoChannel do
     {:ok, resp, assign(socket, :video_id, id)}
 	end
 
-	def handle_in("new_annotation", params, socket) do
-		user = socket.assigns.current_user
+  def handle_in(event, params, socket) do
+    user = socket.assigns.current_user
+    handle_in(event, params, user, socket)
+  end
+
+	def handle_in("new_annotation", params, user, socket) do
 		changeset =
 		  user
 		  |> build(:annotations, video_id: socket.assigns.video_id)
 		  |> Rumbl.Annotation.changeset(params)
 
 		case Repo.insert(changeset) do
-		  {:ok, annotation} ->
-				broadcast! socket, "new_annotation", %{
-				user: Rumbl.UserView.render("user.json", %{user: user}),
-        body: annotation.body,
-        at: annotation.at
-        }
+		  {:ok, ann} ->
+				broadcast_annotation(socket, ann)
+        Task.start_link(fn -> compute_additional_info(ann, socket) end)
         {:reply, :ok, socket}
 			{:error, changeset} ->
 				{:reply, {:error, %{errors: changeset}}, socket}
 		end
   end 
+
+  def broadcast_annotation(socket, annotation) do
+    annotation = Repo.preload(annotation, :user)
+    rendered_ann = Phoenix.View.render(AnnotationView, "annotation.json", %{annotation: annotation})
+    broadcast! socket, "new_annotation", rendered_ann
+  end  
+
+  defp compute_additional_info(ann, socket) do
+    for result <- Rumbl.InfoSys.compute(ann.body, limit: 1) do
+      attrs = %{url: result.url, body: result.text, at: ann.at}
+      info_changeset =
+        Repo.get_by!(Rumbl.User, username: result.backend)
+        |> build(:annotations, video_id: ann.video_id)
+        |> Rumbl.Annotation.changeset(attrs)
+      case Repo.insert(info_changeset) do
+        {:ok, info_ann} -> broadcast_annotation(socket, info_ann)
+        {:error, _changeset} -> :ignore
+      end  
+    end
+  end
 end
