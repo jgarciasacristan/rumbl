9af358a6948fed51aa6a43d00a7638e3937e5c09
Jose Garcia-Sacristan <jose.garciascristan@gmail.com>
2016-01-08 21:37:20 +0000

    update to elixir 1.2 and phoenix 1.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rumbl.Mixfile do
   def project do
     [app: :rumbl,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,11 +30,12 @@ defmodule Rumbl.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "~> 1.1.2"},
+     {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
 		 {:comeonin, "~> 1.0"},
      {:sweet_xml, "~> 0.5.0"}]
